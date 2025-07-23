083103e5bef98ac7fcc2580a48af42f05b27541a
nyesha18 <u.nyesha@iitg.ac.in>
2025-07-23 16:52:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,7 @@ setup(
     packages=['visage'],
     version='0.15.2',
     description='Add virtual makeup to an image of a face.',
-    author='Hriddhi Dey',
-    author_email='hriddhidey@gmail.com',
+   
     url='https://github.com/hriddhidey/visage.git',
     download_url='https://github.com/hriddhidey/visage/archive/0.15.2.tar.gz',
     install_requires=[
